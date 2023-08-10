17b556b0927b95b9e953ac0ac921805db5f0b1f8
xyyang0 <xyyang0@126.com>
2023-08-10 21:22:49 +0800

    fix hard-code

--- src/Address.cpp
+++ src/Address.cpp
@@ -1,4 +1,5 @@
 #include "../headers/Address.hpp"
+#include <string>
 #include <cstring>
 Address::Address():len(sizeof(addr)){
 
@@ -8,7 +9,7 @@ Address::~Address(){
 }
 Address::Address(const char *ip,const char *port):len(sizeof(addr)){
     bzero(&addr,sizeof(addr));
-    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    addr.sin_addr.s_addr = inet_addr(ip);
     addr.sin_family = PF_INET;
-    addr.sin_port = htons(12345);
+    addr.sin_port = htons(std::stoul(port));
 }
