5d830a0d549079350c937f61d16148c19b44f873
xyyang0 <xyyang0@126.com>
2023-08-20 18:26:35 +0800

    Epoll.cpp only return active events

--- src/Epoll.cpp
+++ src/Epoll.cpp
@@ -20,40 +20,18 @@ void Epoll::addFd(int fd,int flags){
     epoll_ctl(epollfd, EPOLL_CTL_ADD, fd, &ev);
 }
 
-
-void readWriteEvents(http *ht){
-    ht->process_routine();
+void Epoll::modfd(int fd, int flags){
+    struct epoll_event ev;
+    ev.data.fd = fd;
+    ev.events = flags;
+    epoll_ctl(epollfd, EPOLL_CTL_MOD, fd, &ev);
 }
 
-void Epoll::eventLoop(const TcpSocket &servSock){
-    while(1){
+std::vector<epoll_event> Epoll::eventLoop(){
+    std::vector<epoll_event> pollEvents;
         int nfds = epoll_wait(epollfd, events, MAX_EVENTS, -1);
         for(int i = 0; i < nfds; i++){
-            if(events[i].data.fd == servSock.getFd()){
-                int acceptfd = servSock.accept();
-                if(acceptfd > 0){
-                    setnonblock(acceptfd);
-                    addFd(acceptfd, EPOLLIN | EPOLLET);
-                }
-            }else if(events[i].events | EPOLLIN){
-                http* ht = nullptr;
-                for(http *item : http::httpConns){
-                    if(item->getFd() == events[i].data.fd){
-                        ht = item;
-                        break;
-                    }
-                    else if(!item->connState()){
-                        ht = item;
-                        ht->setFd(events[i].data.fd);
-                        break;
-                    }
-                }
-                if(!ht){
-                    ht = new http{events[i].data.fd};
-                    http::httpConns.insert(ht);
-                }
-                thPool.addTask(readWriteEvents,ht);
-            }
+            pollEvents.push_back(events[i]);
         }
-    }
+        return pollEvents;
 }
